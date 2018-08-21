60ecb6d791b8a17e222bcfce978edea81c3ade93
kaifeng <kaifeng_5liu@163.com>
2018-08-21 16:30:28 +0800

    首次提交

---
+++ lkf-cloud-gateway/src/main/java/com/lkf/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.lkf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Bean;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+
+/**
+ * @author kaifeng
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
