fbee3ce0ebbbe855c60ea150c98cf7bffce3fd9a
刘凯峰 <liukaifeng@tcsl.com.cn>
2018-08-21 17:48:14 +0800

    注册中心启动成功

--- lkf-cloud-eureka/src/main/java/com/lkf/EurekaApplication.java
+++ lkf-cloud-eureka/src/main/java/com/lkf/EurekaApplication.java
@@ -2,8 +2,11 @@ package com.lkf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
