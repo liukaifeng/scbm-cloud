74700eebf60e355220667f9649458d2906af0199
kaifeng <kaifeng_5liu@163.com>
2018-09-25 16:20:59 +0800

    spring boot admin 调试

---
+++ lkf-eureka-client/src/main/java/com/lkf/EurekaClientApplication.java
@@ -0,0 +1,17 @@
+package com.lkf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author kaifeng
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
