0e530e57775a70b03e5fbff222f25d954fc5752b
刘凯峰 <liukaifeng@tcsl.com.cn>
2018-10-18 16:43:42 +0800

    添加配置中心服务

--- lkf-cloud-admin/src/main/java/com/lkf/SpringBootAdminApplication.java
+++ lkf-cloud-admin/src/main/java/com/lkf/SpringBootAdminApplication.java
@@ -2,13 +2,11 @@ package com.lkf;
 
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
--- lkf-eureka-client/src/main/java/com/lkf/EurekaClientApplication.java
+++ lkf-eureka-client/src/main/java/com/lkf/EurekaClientApplication.java
@@ -3,12 +3,14 @@ package com.lkf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author kaifeng
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
