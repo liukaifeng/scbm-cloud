ce712f2bf1784b1f49a3f07a950e64c445683e5e
刘凯峰 <liukaifeng@tcsl.com.cn>
2018-12-18 17:22:28 +0800

    动态路由调试

--- lkf-cloud-gateway/gateway-server/src/main/java/com/lkf/gateway/config/Application.java
+++ lkf-cloud-gateway/gateway-server/src/main/java/com/lkf/gateway/config/Application.java
@@ -3,7 +3,6 @@ package com.lkf.gateway.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
@@ -15,9 +14,9 @@ import org.springframework.context.annotation.PropertySource;
 @EnableEurekaClient
 @ComponentScan(basePackages = "com.lkf")
 @PropertySource(value = "classpath:config/application.properties")
-public class CmpGatewayApplication {
+public class Application {
 
     public static void main( String[] args ) {
-        SpringApplication.run(CmpGatewayApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
