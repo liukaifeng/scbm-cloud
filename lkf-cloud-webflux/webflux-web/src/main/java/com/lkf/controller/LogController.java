d4de272c5990d99ed14a4ab8b632eba554a09531
刘凯峰 <kaifeng_5liu@163.com>
2020-04-01 16:14:15 +0800

    log4j2调试

---
+++ lkf-cloud-webflux/webflux-web/src/main/java/com/lkf/controller/LogController.java
@@ -0,0 +1,26 @@
+package com.lkf.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 刘凯峰
+ * @version V1.0
+ * update-logs:方法变更说明
+ * ****************************************************
+ * name:
+ * date:
+ * description:
+ * *****************************************************
+ * @date 2020-04-01 15:56
+ * @description TODO
+ */
+@Slf4j
+public class LogController {
+    public static void main(String... args) {
+        for (int i = 0; i < 1000; i++) {
+            log.error("Something else is wrong here");
+            log.info("something else is info here");
+        }
+    }
+}
