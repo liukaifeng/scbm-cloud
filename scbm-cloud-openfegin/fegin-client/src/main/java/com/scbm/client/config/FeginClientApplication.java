ee1f41e792d42559298866fa49f5a3e8d2104070
刘凯峰 <kaifeng_5liu@163.com>
2020-06-23 13:51:18 +0800

    修改包名

--- scbm-cloud-openfegin/fegin-client/src/main/java/com/scbm/client/config/FeginClientApplication.java
+++ scbm-cloud-openfegin/fegin-client/src/main/java/com/scbm/client/config/FeginClientApplication.java
@@ -3,7 +3,6 @@ package com.scbm.client.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
